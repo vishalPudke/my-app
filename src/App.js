2b81d0cf80bfb8cceaf4d6d020e7433fee81413c
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-13 09:48:43 +0530

    # Practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+//JSX = JS + HTML
+//If  a function Return HTML,This BEcomes A User Defined TAG
+//<App></App>
+//<App/>
+//Compare Pre Defined TAG  :- Lower Case Convention
+//h1, p, li
+//Why Master case convention
+//User Defined Tag  :-Camel Case Convention
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  return <h1>Hello World!</h1>
 }
-
 export default App;
