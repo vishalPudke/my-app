96c3d7f3b2d56d18a9067d5834fafa38125945ee
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-13 14:33:57 +0530

    commit changes hellllloooooo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App(){
   return(
     <>
     <h1>Hello World</h1>
-    <h1>THANK YOU VITHAL</h1>
+    <h1>Thank YOu Everyone</h1>
     <button type="button" className="btn btn-primary">Primary</button>
     </>
   
