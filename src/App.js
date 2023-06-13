e44ad35a865fa94ccc9a104859c5aa04f1c3cf32
vishalPudke <vishal.mechtech19@gmail.com>
2023-06-13 14:35:10 +0530

    thnak you

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App(){
   return(
     <>
     <h1>Hello World</h1>
-    <h1>Thank YOu Everyone</h1>
+    <h1>Thank YOu Everyone FORKINX MAKERS</h1>
     <button type="button" className="btn btn-primary">Primary</button>
     </>
   
